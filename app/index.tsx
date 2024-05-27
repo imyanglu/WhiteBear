f192c3c82c111959fa0fa9d7ff2c643dbcb9cb2f
wii <99741633+imyanglu@users.noreply.github.com>
2024-05-27 14:28:07 +0800

    i

--- app/index.tsx
+++ app/index.tsx
@@ -11,21 +11,23 @@ const Utils = [{
     des: 'QQ音乐,网易云',
     href:'/music',
     renderItem:function () { 
-        return <Pressable className="flex-row items-center px-[12px] w-full bg-[#6EB3A0] h-full py-[8px]  rounded-[8px]" onPress={() => { 
+        return <Pressable className="flex-row items-center px-[12px] w-full bg-[#6EB3A0] h-full py-[8px]  rounded-[8px]" style={{
+            elevation:12, shadowColor: '#000',
+        }} onPress={() => { 
 
             router.push(this.href)
         }}>
                   <MaterialIcons name="my-library-music" size={24} color="#A5E3BA" />
-            <Text className="ml-[12px] text-[#fff] font-bold">{this.label} { this.des}</Text>
+            <Text className="ml-[12px] text-[#fff] font-bold">{this.label} <Text className="text-[12px]">{ this.des}</Text></Text>
         </Pressable>
     }
 }]
 
 const Index = () => { 
     const { top} = useSafeAreaInsets()
-    return <View className='flex-1 px-[16px] flex-row flex-wrap gap-x-[12px]' style={{ paddingTop: top +24}}>
+    return <View className='flex-1 bg-[#f2f2f2] px-[16px] flex-row flex-wrap gap-x-[12px]' style={{ paddingTop: top +24}}>
         {Utils.map((item, index) => {
-            return <View key={item.key} className='flex-row  basis-[50%] bg-[#E0E3EA] items-center justify-center'>
+            return <View key={item.key} className='flex-row  basis-[50%]  items-center justify-center'>
                 {item.renderItem()}
             </View>
         })}
